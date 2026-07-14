d731e1d7aba5768e96df493d6b0f3c518134c9aa
nino <nino.paichadze2003@gmail.com>
2026-07-14 12:35:13 +0400

    chore: clean up csproj file removing obj and bin references

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+284f5e89ca03b897fde7304af0e501787c7cef99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb099ec64cc80d7e80713af4e4114eed3209f38d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
